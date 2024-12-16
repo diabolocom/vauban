635e8de7f62ec316347c5607289364f5a321904d
Cyril Duval <cyril@fayak.com>
2024-12-16 16:50:01 +0100

    vauban-http-server: better JSON output on /status endpoint

--- http-server/vauban-http-server/kube.py
+++ http-server/vauban-http-server/kube.py
@@ -35,6 +35,10 @@ spec:
   backoffLimit: 1
 """
     job_manifest = yaml.safe_load(job)
-    command = ["bash", "-c", f"cp /opt/vauban/.secrets.env /srv ; cp /opt/vauban/ansible_id_ed25519 /srv/ansible-ro ; chmod 0600 /srv/ansible-ro ; mkdir -p /root/.ssh ; cp /opt/vauban/id_ed25519 /root/.ssh/ ; chmod 0600 /root/.ssh/id_ed25519 ; export VAUBAN_KUBERNETES_ENGINE_PUB_KEY=\"$(cat /opt/vauban/id_ed25519.pub)\" ; {' '.join(command)}"]
-    job_manifest['spec']['template']['spec']['containers'][0]['command'] = command
+    command = [
+        "bash",
+        "-c",
+        f"cp /opt/vauban/.secrets.env /srv ; cp /opt/vauban/ansible_id_ed25519 /srv/ansible-ro ; chmod 0600 /srv/ansible-ro ; mkdir -p /root/.ssh ; cp /opt/vauban/id_ed25519 /root/.ssh/ ; chmod 0600 /root/.ssh/id_ed25519 ; export VAUBAN_KUBERNETES_ENGINE_PUB_KEY=\"$(cat /opt/vauban/id_ed25519.pub)\" ; {' '.join(command)}",
+    ]
+    job_manifest["spec"]["template"]["spec"]["containers"][0]["command"] = command
     return job_manifest
--- http-server/vauban-http-server/main.py
+++ http-server/vauban-http-server/main.py
@@ -1,11 +1,24 @@
 from flask import Flask
-from flask import request
+from flask import request, jsonify
 import os
 from kubernetes import client, config, utils
 from kubernetes.client.rest import ApiException
 from kubernetes.stream import stream
 from ulid import ULID
 from .kube import get_vauban_job
+import sentry_sdk
+
+SENTRY_DSN = os.environ.get("SENTRY_DSN", None)
+if SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[
+            FlaskIntegration(
+                transaction_style="url",
+            ),
+        ],
+        enable_tracing=False,
+    )
 
 app = Flask(__name__)
 
@@ -22,63 +35,152 @@ core_api_instance = client.CoreV1Api(k8s_client)
 batch_api_instance = client.BatchV1Api(k8s_client)
 namespace = os.environ.get("VAUBAN_NAMESPACE", "vauban")
 
+
 @app.route("/build", methods=["POST"])
 def build():
     try:
         args = request.get_json(force=True)
     except Exception as e:
         print(f"Exception in get_json: {e}")
-        return "Need some arguments man\n", 400
+        return (
+            jsonify({"status": "error", "message": "Need some JSON arguments man"}),
+            400,
+        )
     ulid = str(ULID())
     try:
         vauban_cli = [
             "./vauban.py",
             "--name",
-            args['name'],
+            args["name"],
             "--stage",
-            args['stage'],
-            ]
+            args["stage"],
+        ]
     except KeyError as e:
-        return f"Need some more arguments man\nmissing: {e}\n", 400
+        return (
+            jsonify(
+                {
+                    "status": "error",
+                    "message": f"Need some more arguments. missing: {e}\n",
+                }
+            ),
+            400,
+        )
     for arg in ("build-parents", "branch", "conffs"):
         if arg in args:
             vauban_cli += [f"--{arg}", args[arg]]
     if "extra-args" in args:
         vauban_cli += [args["extra-args"]]
     # FIXME make it configurable
-    manifest = get_vauban_job(ulid, args.get("vauban-image", "zarakailloux/vauban:latest"), vauban_cli)
+    manifest = get_vauban_job(
+        ulid,
+        args.get("vauban-image", "zarakailloux/vauban:latest"),
+        vauban_cli,
+    )
 
     try:
         api_response = batch_api_instance.create_namespaced_job(namespace, manifest)
     except ApiException as e:
-        return f"Failed to run a new job: {e}\n"
-    return ulid
+        return (
+            jsonify({"status": "error", "message": f"Failed to run a new job: {e}\n"}),
+            500,
+        )
+    return jsonify({"status": "ok", "job_ulid": ulid})
+
+
+def _get_logs(ulid):
+    try:
+        pods_response = core_api_instance.list_namespaced_pod(
+            namespace,
+            label_selector=f"vauban.corp.dblc.io/vauban-job-id={ulid.upper()}",
+        )
+    except ApiException as e:
+        pods_response = None
+    pods = {}
+    if pods_response is not None:
+        for pod in pods_response.items:
+            pods[pod.metadata.creation_timestamp] = {
+                "name": pod.metadata.name,
+                "status": pod.status.container_statuses[0].state,
+            }
+    log_list = []
+    for key in sorted(pods.keys()):
+        pod = pods[key]
+        try:
+            pod_log = core_api_instance.read_namespaced_pod_log(
+                pod["name"], namespace, tail_lines=150
+            )
+            log_obj = {"status": "ok", "logs": pod_log}
+        except ApiException as e:
+            error_msg = f"Error while getting pod logs: {e}"
+            log_obj = {"status": "error", "message": error_msg}
+        log_list.append(log_obj)
+    return {"logs": log_list[-1], "previous_pods_logs": log_list[:-1]}
+
 
 @app.route("/status/<ulid>")
 def status(ulid):
     try:
-        api_response = batch_api_instance.read_namespaced_job(f"vauban-{ulid.lower()}", namespace)
+        api_response = batch_api_instance.read_namespaced_job(
+            f"vauban-{ulid.lower()}", namespace
+        )
     except ApiException as e:
-        return f"Error while trying to get jod: {e}"
+        return (
+            jsonify(
+                {"status": "error", "message": f"Error while trying to get jod: {e}"}
+            ),
+            500,
+        )
+    log_objs = _get_logs(ulid)
     status = api_response.status
-    if status.active is None and status.completion_time is None and status.failed > 0 and status.ready == 0:
-        return "Job failed !"
-        # FIXME pod logs
-    return f"Job in an unknown state: {str(status)}"
+    if (
+        status.active is None
+        and status.completion_time is None
+        and status.failed > 0
+        and status.ready == 0
+    ):
+        return jsonify({"status": "error", "message": "Job failed !"} | log_objs), 500
+    if status.active is not None:
+        return (
+            jsonify(
+                {"status": "in-progress", "message": "Job is currently running"}
+                | log_objs
+            ),
+            202,
+        )
+    if status.active is None and status.completion_time is not None:
+        return jsonify(
+            {"status": "ok", "message": "Job is done ! Build successful"} | log_objs
+        )
+    return (
+        jsonify(
+            {"status": "unknown", "message": f"Job in an unknown state: {str(status)}"}
+        ),
+        500,
+    )
+
 
 @app.route("/delete/<ulid>", methods=["DELETE", "POST"])
 def delete(ulid):
     try:
-        api_response = batch_api_instance.delete_namespaced_job(f"vauban-{ulid.lower()}", namespace, propagation_policy="Background")
+        api_response = batch_api_instance.delete_namespaced_job(
+            f"vauban-{ulid.lower()}", namespace, propagation_policy="Background"
+        )
     except ApiException as e:
         if str(e.status) == "404":
-            return "Already deleted !", 200
-        return f"Error while trying to get jod: {e}", 400
-    return "Deleted !", 201
+            return jsonify({"status": "ok", "message": "Already deleted !"}), 200
+        return (
+            jsonify(
+                {"status": "error", "message": f"Error while trying to get jod: {e}"}
+            ),
+            500,
+        )
+    return jsonify({"status": "ok", "message": "Deleted !"}), 201
+
 
 @app.route("/readiness")
 def readiness():
     return "ok"
+
 
 @app.route("/health")
 def health():
